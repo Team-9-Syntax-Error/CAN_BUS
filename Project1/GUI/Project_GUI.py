068db2caa0366b4110c6c99fd4996ef2bbbc7e03
Joseph BAca <jojobaca123@gmail.com>
2022-09-14 20:46:51 -0600

    First Push for initial GUI

---
+++ Project1/GUI/Project_GUI.py
@@ -0,0 +1,44 @@
+
+# Creating simple GUI to demostrate SYNC capabilities.
+
+import tkinter
+from tkinter import Label, Button, Tk
+from PIL import ImageTk, Image
+import webbrowser
+import shutil
+import os
+
+
+# TODO: Button for project config
+#       Button to view database in finder
+#       Button for importing .Json
+#       
+
+class CAN_BUS_GUI:
+    def __init__(self, master) -> None:
+
+        self.pwd = os.getcwd()
+        self.master = master
+        master.title("CAN BUS")
+
+        # Setting up Texts
+        self.label = Label(master, text="CAN BUS")
+        self.label.pack()
+
+
+        # Read Button file for Analyst 1
+        self.File_button_A1 = Button(master, text="Database Directory")
+        self.File_button_A1.pack()
+        self.File_button_A1.place(x=150, y=75)
+
+        # Read Button file for Analyst 2
+        self.File_button_A2 = Button(master, text="Project Configurations")
+        self.File_button_A2.pack()
+        self.File_button_A2.place(x=525, y=75)
+
+
+
+root = Tk()
+my_gui = CAN_BUS_GUI(root)
+root.geometry("750x500")
+root.mainloop()
