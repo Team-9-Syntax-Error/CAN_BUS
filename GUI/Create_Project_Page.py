cbf8d4178e752fbc571d8964b78a4d0b7eb84197
mjavila4 <mjavila4@miners.utep.edu>
2022-09-19 10:51:43 -0600

    Changed button and label placements. Added background image.

--- GUI/Create_Project_Page.py
+++ GUI/Create_Project_Page.py
@@ -1,10 +1,9 @@
 import tkinter
-from tkinter import Label, Button, Tk, Frame, Text, font
+from tkinter import Label, Button, Tk, Frame, Text, font, PhotoImage, Canvas
 from Data_Manager import DataManager
 
 
 class Create_Project_Frame(Frame):
-
     # Initialize Text
     analyst_initials_text = Text
     event_name_text = Text
@@ -19,35 +18,51 @@ class Create_Project_Frame(Frame):
         # Get CONFIG Data
         self.data_manager = DataManager()
         self.config_data = self.data_manager.get_config_data()
+        self.bg_img = PhotoImage(file="Images/Background.png")
+        self.pr = parent
 
         Frame.__init__(self, parent)
         self.label_font = font.Font(family="Helvetica", size=15, weight="bold")
         self.text_font = font.Font(family="Helvetica", size=15)
+        self.title_font = font.Font(family="Helvetica", size=20, weight="bold")
+        self.button_font = font.Font(weight="bold", size=11)
         self.controller = controller
         self.place_labels()
         self.place_text()
         self.place_buttons()
 
     def retrieve_data(self):
-        self.config_data['Project Configuration'].update({'Analyst Initials': self.analyst_initials_text.get("1.0", 'end-1c')})
+        self.config_data['Project Configuration'].update(
+            {'Analyst Initials': self.analyst_initials_text.get("1.0", 'end-1c')})
         self.config_data['Project Configuration'].update({'Event Name': self.event_name_text.get("1.0", 'end-1c')})
         self.config_data['Project Configuration'].update({'Event Date': self.event_date_text.get("1.0", 'end-1c')})
         self.config_data['Project Configuration'].update({'Can Connector ID': self.cc_id_text.get("1.0", 'end-1c')})
         self.config_data['Project Configuration'].update({'Vehicle ID': self.v_id_text.get("1.0", 'end-1c')})
         self.config_data['Project Configuration'].update({'Baud Rate': self.baud_rate_text.get("1.0", 'end-1c')})
         self.config_data['Project Configuration'].update({'DBC File Name': self.dbc_filename_text.get("1.0", 'end-1c')})
-        self.config_data['Project Configuration'].update({'Black List File Name': self.bl_filename_text.get("1.0", 'end-1c')})
+        self.config_data['Project Configuration'].update(
+            {'Black List File Name': self.bl_filename_text.get("1.0", 'end-1c')})
         # Send User-Input to Data Manager
         self.data_manager.receive_user_config_data(self.config_data)
 
     def place_buttons(self):
-        button_first_page = Button(self, text="First Page",command=lambda: self.controller.show_frame("First_Page_Frame"))
-        button_first_page.pack()
+        back_button = Button(self, text="Back", activebackground="light blue", font=self.button_font,
+                             command=lambda: self.controller.show_frame("First_Page_Frame"))
+        back_button.pack()
+        back_button.place(x=10, y=10)
 
-        button_save = Button(self, text="Save Page", command=lambda: self.retrieve_data())
+        button_save = Button(self, text="Create Project", activebackground="light blue", font=self.button_font,
+                             command=lambda: self.retrieve_data())
         button_save.pack()
+        button_save.place(x=315, y=464)
 
     def place_labels(self):
+        background_label = Label(self, image=self.bg_img)
+        background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+        title = Label(self, text="Project Configuration", font=self.title_font)
+        title.place(x=230, y=15)
+
         analist_initials = Label(self, text="Analyst Initials:", font=self.label_font)
         analist_initials.place(x=200, y=75)
 
