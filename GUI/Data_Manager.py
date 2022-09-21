dc862b81962f643227817f047b4d34a746bb06c4
Joseph BAca <jojobaca123@gmail.com>
2022-09-20 21:25:34 -0600

    works on mac now -JB

--- GUI/Data_Manager.py
+++ GUI/Data_Manager.py
@@ -57,9 +57,10 @@ class DataManager:
 
             # Copy over Node_Info
             os.chdir("../")
-            shutil.copy(os.getcwd() + "\\Default_Data_Config\\Node_info.json", os.getcwd()+"/"+folder_name)
+            print("*******************************************"+ os.getcwd())
+            shutil.copy(os.getcwd() + "/Default_Data_Config/Node_info.json", os.getcwd()+"/"+folder_name)
             # Copy over Packet_Info
-            shutil.copy(os.getcwd() + "\\Default_Data_Config\\Packet_info.json", os.getcwd()+"/"+folder_name)
+            shutil.copy(os.getcwd() + "/Default_Data_Config/Packet_info.json", os.getcwd()+"/"+folder_name)
 
         else:
             return False
