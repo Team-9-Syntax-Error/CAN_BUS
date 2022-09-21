1d8daf3aa911fc9756ba4c9b59b9b76d663fe9b2
Kevin Nathaniel Tiscareno Chavez <mygame1980@hotmail.com>
2022-09-21 10:21:22 -0600

    Template image for Project

--- GUI/Home_Page.py
+++ GUI/Home_Page.py
@@ -1,11 +1,17 @@
 
 from re import fullmatch
-from tkinter import LEFT, TOP, Label, Button, Menu, Tk, Frame, filedialog, PhotoImage
+from tkinter import BOTH, LEFT, TOP, Canvas, Label, Button, Menu, Tk, Frame, filedialog, PhotoImage
 import os
 
 #from matplotlib.font_manager import json_dump
 from First_Page import First_Page_Frame
 import json
+
+#Path
+path = os.path.dirname(os.path.realpath(__file__))
+os.chdir(path)
+full_path = path + "/Images/"
+canCar_path = full_path + "CAN Car.png"
 
 
 class Home_Page_Frame(Frame):
@@ -13,11 +19,18 @@ class Home_Page_Frame(Frame):
         Frame.__init__(self, parent)
 
         self.controller = controller
-
         
         current_project_directory = self.current_project_directory()
         lable = Label(self, text = "Home Page", bg="#00528C", font="Helvetica, 32")
         lable.pack(side="top", fill="x", pady=10)
+
+      #
+        self.carImage = PhotoImage(file = canCar_path)
+      
+      #This handles the CAN bus image
+        canBus = Canvas(self, width=20, height=20)
+        canBus.create_image(500, 100, image = self.carImage) #Handle Image
+        canBus.pack(expand=True, fill=BOTH)
 
           #Menu
         menu1 = Menu(controller)
@@ -32,8 +45,6 @@ class Home_Page_Frame(Frame):
 
         menu1.add_cascade(label = "Packet", menu = submenu1)
         submenu1.add_command(label = "Show", command = "Modificar//")
-
-
 
         
     def current_project_directory(self):
