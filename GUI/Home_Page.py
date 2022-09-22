b4a4a446f387233ceca38c7f7b780ed1cc5744bd
ruorozco <90886186+ruorozco@users.noreply.github.com>
2022-09-22 08:06:52 -0600

    homepage

--- GUI/Home_Page.py
+++ GUI/Home_Page.py
@@ -1,6 +1,6 @@
 
 from re import fullmatch
-from tkinter import BOTH, LEFT, TOP, Canvas, Label, Button, Menu, Tk, Frame, filedialog, PhotoImage
+from tkinter import BOTH, CENTER, LEFT, TOP, Canvas, Label, Button, Menu, Tk, Frame, filedialog, PhotoImage, ttk
 import os
 
 #from matplotlib.font_manager import json_dump
@@ -14,16 +14,33 @@ full_path = path + "/Images/"
 canCar_path = full_path + "CAN Car.png"
 
 
+
 class Home_Page_Frame(Frame):
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
 
         self.controller = controller
         
-        current_project_directory, current_project_title = self.current_project_directory()
-        lable = Label(self, text = current_project_title, bg="#00528C", font="Helvetica, 32")
+        #current_project_directory, current_project_title = self.current_project_directory()
+        lable = Label(self, text = "Home Page", bg="gray", font="Helvetica, 32")
+        lable.pack(side="top", fill="x", pady=10)
+        
+        lable = Label(self, text = "Traffic", bg="white", font="Helvetica, 16")
         lable.pack(side="top", fill="x", pady=10)
 
+        tree = ttk.Treeview(self, column=("Id", "Time", "Data"), show='headings', height=2)
+        tree.column("# 1", anchor=CENTER)
+        tree.heading("# 1", text="ID")
+        tree.column("# 2", anchor=CENTER)
+        tree.heading("# 2", text="TIME")
+        tree.column("# 3", anchor=CENTER)
+        tree.heading("# 3", text="DATE")
+
+        #tree.column("Id",width= 200) #Not necesari at the moment
+        tree.pack()
+
+        lable = Label(self, text = "CAN Map", bg="white", font="Helvetica, 16")
+        lable.pack(side="top", fill="x", pady=10)
       #
         self.carImage = PhotoImage(file = canCar_path)
       
@@ -35,17 +52,33 @@ class Home_Page_Frame(Frame):
         #Menu
         menu1 = Menu(controller)
         controller.config(menu = menu1)
-
         submenu1 = Menu(menu1)
+        submenu2 = Menu(menu1)
+        submenu3 = Menu(menu1)
+        submenu4 = Menu(menu1)
+        submenu5 = Menu(menu1)
+      
         menu1.add_cascade(label = "File", menu = submenu1)
+        submenu1.add_command(label = "New", command = "Modificar//")
+        submenu1.add_command(label = "Open", command = "Modificar//")
         submenu1.add_command(label = "Save", command = "Modificar//")
 
-        menu1.add_cascade(label = "View", menu = submenu1)
-        submenu1.add_command(label = "Somethin", command = "Modificar//")
+        menu1.add_cascade(label = "View", menu = submenu2)
+        submenu2.add_command(label = "Zoom-In", command = "Modificar//")
+        submenu2.add_command(label = "Zoom-Out", command = "Modificar//")
 
-        menu1.add_cascade(label = "Packet", menu = submenu1)
-        submenu1.add_command(label = "Show", command = "Modificar//")
-
+        menu1.add_cascade(label = "Packet", menu = submenu3)
+        submenu3.add_command(label = "Show", command = "Modificar//")
+        
+        menu1.add_cascade(label = "Edit", menu = submenu4)
+        submenu4.add_command(label = "Add", command = "Modificar//")
+        submenu4.add_command(label = "Remove", command = "Modificar//")
+        submenu4.add_command(label = "Connect", command = "Modificar//")
+        
+        menu1.add_cascade(label = "Nodes", menu = submenu5)
+        submenu5.add_command(label = "Show", command = "Modificar//")
+        submenu5.add_command(label = "Hide", command = "Modificar//")
+        submenu5.add_command(label = "Highlight", command = "Modificar//")
         
     def current_project_directory(self):
         with open("../User_Database/Current_Project.json", 'r') as openfile:
